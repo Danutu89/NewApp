34ea4e3f30a08125099dc63315d911964c6ff047
Danutu <dany89yt@yahoo.com>
2019-07-03 19:05:53 +0200

    .

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ def load_user(user_id):
 
 @app.route('/favicon.ico')
 def favicon():
+    print (os.path.join(app.root_path, 'static'))
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
